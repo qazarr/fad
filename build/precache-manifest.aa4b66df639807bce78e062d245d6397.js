77b9bcfe0fe1159c23c3ddab6cca6d8ead4c99e4
Damian Makarow <damian.makarow@makolab.net>
2019-01-20 22:52:55 +0100

    first build

---
+++ build/precache-manifest.aa4b66df639807bce78e062d245d6397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/fad/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "e29cb10a308057a19f98",
+    "url": "/fad/static/js/runtime~main.e29cb10a.js"
+  },
+  {
+    "revision": "0c0967ab6fdc64c48211",
+    "url": "/fad/static/js/main.0c0967ab.chunk.js"
+  },
+  {
+    "revision": "d5f7de5f1f9375eaaace",
+    "url": "/fad/static/js/1.d5f7de5f.chunk.js"
+  },
+  {
+    "revision": "0c0967ab6fdc64c48211",
+    "url": "/fad/static/css/main.1531263a.chunk.css"
+  },
+  {
+    "revision": "d5f7de5f1f9375eaaace",
+    "url": "/fad/static/css/1.5e7ff27e.chunk.css"
+  },
+  {
+    "revision": "52b94f3e6de98dd8ae12ebe444b0d873",
+    "url": "/fad/index.html"
+  }
+];
